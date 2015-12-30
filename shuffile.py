2189c70f2c0a85c57877dfa493b17c7cb96e8c69
William H. Grover <wgrover@engr.ucr.edu>
2015-12-30 15:05:38 -0400

    Directory now specified on command line, not hard-coded

--- shuffile.py
+++ shuffile.py
@@ -1,14 +1,21 @@
-import random, os
+import random, os, sys
 
-dirname = "/Users/wgrover/Documents/Classes/BIEN 167 UG Medical Diagnostics/2015/Mini-project 2 final slides"
-files = os.listdir(dirname)
-random.shuffle(files)
-counter = 0
-for filename in files:
-    if not filename.startswith("."):
-        print counter, filename
-        os.rename(os.path.join(dirname,filename), os.path.join(dirname,str(counter).zfill(2)+filename))
-        counter += 1
+if len(sys.argv) != 2:
+    print """
+    USAGE:
+        python shuffile.py directory_name
+    where 'directory_name' is the full path of the directory containing files to be shuffled.
+    """
+else:
+    dirname = sys.argv[1]
+    files = os.listdir(dirname)
+    random.shuffle(files)
+    counter = 0
+    for filename in files:
+        if not filename.startswith("."):
+            print counter, filename
+            os.rename(os.path.join(dirname,filename), os.path.join(dirname,str(counter).zfill(2)+filename))
+            counter += 1
 
 
 
